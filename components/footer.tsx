46e17cc96cbbda47d54541827bb659255c859573
Stuart Izon <stuart.izon@gmail.com>
2021-12-31 02:19:45 +1100

    Add Apple and Amazon links to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faYoutube, faSpotify } from "@fortawesome/free-brands-svg-icons";
+import { faAmazon, faApple, faFacebook, faTwitter, faYoutube, faSpotify } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
     return (
@@ -11,12 +11,19 @@ const Footer = () => {
                 <a href="https://twitter.com/shabbatonchoir" target="_blank" rel="noreferrer">
                     <FontAwesomeIcon size="2x" className="text-gray-600" icon={faTwitter} />
                 </a>
-                <a href="https://open.spotify.com/artist/3UZxQiOiQL8bgl4Zc4R9vk" target="_blank" rel="noreferrer">
-                    <FontAwesomeIcon size="2x" className="text-gray-600" icon={faSpotify} />
-                </a>
                 <a href="https://www.youtube.com/user/ShabbatonChoir" target="_blank" rel="noreferrer">
                     <FontAwesomeIcon size="2x" className="text-gray-600" icon={faYoutube} />
                 </a>
+                <a href="https://music.apple.com/gb/artist/shabbaton-choir/449897420" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon size="2x" className="text-gray-600" icon={faApple} />
+                </a>
+                <a href="https://open.spotify.com/artist/3UZxQiOiQL8bgl4Zc4R9vk" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon size="2x" className="text-gray-600" icon={faSpotify} />
+                </a>
+                <a href="https://music.amazon.co.uk/artists/B005CSNERG/shabbaton-choir" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon size="2x" className="text-gray-600" icon={faAmazon} />
+                </a>
+
             </div>
         </footer>
     );
