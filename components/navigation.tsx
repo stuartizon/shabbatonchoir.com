6111295fa18f929c08ed21b9df6bd75e751d225d
Stuart Izon <stuart.izon@gmail.com>
2022-08-02 10:40:42 -0400

    Disable keyboard tabbing for mobile nav links

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -6,11 +6,11 @@ const Navigation: FC<NavigationProps> = ({ visible }) => (
     <nav className="relative">
         <div className={cn("absolute top-0 right-0 bottom-0 w-screen duration-500 z-10", { "translate-x-full": !visible, "translate-x-0": visible })}>
             <div className="bg-white flex flex-col space-y-4 px-2 py-6 uppercase text-center">
-                <Link href="/"><a className="text-sm font-medium">Home</a></Link>
-                <Link href="/music"><a className="text-sm font-medium">Music</a></Link>
-                <Link href="/leadership"><a className="text-sm font-medium">Leadership</a></Link>
-                <Link href="/history"><a className="text-sm font-medium">History</a></Link>
-                <Link href="/contact"><a className="text-sm font-medium">Contact</a></Link>
+                <Link href="/"><a className="text-sm font-medium" tabIndex={-1}>Home</a></Link>
+                <Link href="/music"><a className="text-sm font-medium" tabIndex={-1}>Music</a></Link>
+                <Link href="/leadership"><a className="text-sm font-medium" tabIndex={-1}>Leadership</a></Link>
+                <Link href="/history"><a className="text-sm font-medium" tabIndex={-1}>History</a></Link>
+                <Link href="/contact"><a className="text-sm font-medium" tabIndex={-1}>Contact</a></Link>
             </div>
         </div>
     </nav>
